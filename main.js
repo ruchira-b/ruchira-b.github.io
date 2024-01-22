61f622bf605e001bd9fe764aa1e583eb4151cd8c
ruchira-b <143113252+ruchira-b@users.noreply.github.com>
2024-01-22 18:02:03 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+document.querySelector("html").addEventListener("click", () => {
+    alert("Ouch! Stop poking me!");
+  });
+  
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+      myImage.setAttribute("src", "images/firefox2.png");
+    } else {
+      myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+  };
+  
